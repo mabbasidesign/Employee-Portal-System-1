4e23afb5c4283bda3dc410afd478d7cce123f32f
Mohsen Abbasi <mabbasidesign@yahoo.com>
2017-11-18 12:51:38 -0700

    Start

---
+++ Employee Portal System 1/Employee Portal System 1/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Employee_Portal_System_1.MvcApplication" Language="C#" %>
---
+++ Employee Portal System 1/Employee Portal System 1/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Employee_Portal_System_1.Startup))]
+namespace Employee_Portal_System_1
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
