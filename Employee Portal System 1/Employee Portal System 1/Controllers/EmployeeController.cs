cb748e8e207588695fce4d27fe2e8a036bf6ef7a
Mohsen Abbasi <mabbasidesign@yahoo.com>
2017-11-18 14:59:14 -0700

    Add DropDown for Department

---
+++ Employee Portal System 1/Employee Portal System 1/Controllers/EmployeeController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Employee_Portal_System_1.Models;
+
+namespace Employee_Portal_System_1.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        // GET: Employee
+        public ActionResult Index(EmployeeViewModel model)
+        {
+            var db = new EmEntities();
+            var depList = db.Departments.ToList();
+            ViewBag.DepartmentList = new SelectList(depList, "DepartmentId", "DepartmentName");
+
+            var em = new Employee();
+            em.EmployeeId = model.EmployeeId;
+            em.Name = model.Name;
+            em.Address = model.Address;
+            em.DepartmentId = model.DepartmentId;
+
+            var emList = db.Employees.Add(em);
+            db.SaveChanges();
+
+
+
+            return View();
+        }
+    }
+}
