88d0da12c706ea30e490e74d910183b9155dde08
Mohsen Abbasi <mabbasidesign@yahoo.com>
2017-11-19 13:26:27 -0700

    Add View and controller for delete employee

--- Employee Portal System 1/Employee Portal System 1/Controllers/DeleteEmployeeController.cs
+++ Employee Portal System 1/Employee Portal System 1/Controllers/DeleteEmployeeController.cs
@@ -1,42 +1,16 @@
-﻿using System;
+﻿using Employee_Portal_System_1.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-using Employee_Portal_System_1.Models;
 
 namespace Employee_Portal_System_1.Controllers
 {
-    public class EmployeeController : Controller
+    public class DeleteEmployeeController : Controller
     {
-        // GET: Employee
-        public ActionResult Index(EmployeeViewModel model)
-        {
-            var db = new EmEntities();
-            var depList = db.Departments.ToList();
-            ViewBag.DepartmentList = new SelectList(depList, "DepartmentId", "DepartmentName");
 
-            var em = new Employee();
-            //em.EmployeeId = model.EmployeeId;
-            em.Name = model.Name;
-            em.Address = model.Address;
-            em.DepartmentId = model.DepartmentId;
-            db.Employees.Add(em);
-            //db.SaveChanges();
-
-            var latestEmpId = em.EmployeeId;
-            
-            var site = new Site();
-            site.SiteName = model.SiteName;
-            db.Sites.Add(site);
-            //db.SaveChanges();
-
-            site.EmployeeId = latestEmpId;
-
-            return View(model);
-        }
-
-        public ActionResult DeleteEmployee()
+        public ActionResult Index()
         {
             var db = new EmEntities();
 
@@ -60,9 +34,9 @@ namespace Employee_Portal_System_1.Controllers
         {
             var result = false;
             var db = new EmEntities();
-          
+
             var em = db.Employees.SingleOrDefault(e => e.EmployeeId == id && e.IsDeleted == false);
-            if(em != null)
+            if (em != null)
             {
                 em.IsDeleted = true;
                 result = true;
@@ -71,7 +45,6 @@ namespace Employee_Portal_System_1.Controllers
 
             return Json(result, JsonRequestBehavior.AllowGet);
         }
-
 
     }
 }
